6afff0af74a5ea6558f3e7e4f715378e4399814b
jaengs <96903573+ItsMeJaeng@users.noreply.github.com>
2022-03-11 00:30:26 +0900

    0311

---
+++ 596_Classes_More_Than_5_Students.sql
@@ -0,0 +1,5 @@
+--LeetCode 596. Classes More Than 5 Students
+select class
+from courses
+group by class
+having count(student) >= 5
---
+++ 620_Not_Boring_Movies.sql
@@ -0,0 +1,5 @@
+--LeetCode 620. Not Boring Movies
+select *
+from cinema
+where mod(id,2) <> 0 and description <> 'boring'
+order by rating desc;
